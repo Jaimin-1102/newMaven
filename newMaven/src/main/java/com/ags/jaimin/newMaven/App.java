9551fab882a0b77f09d816a3dc38697bd3104519
Jaimin-1102 <jaimin.19beceg120@gmail.com>
2022-08-04 11:21:56 +0530

    Initial commit

---
+++ newMaven/src/main/java/com/ags/jaimin/newMaven/App.java
@@ -0,0 +1,13 @@
+package com.ags.jaimin.newMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
